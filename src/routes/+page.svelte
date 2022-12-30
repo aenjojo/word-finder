717b67704e7588c834c98a45d36faadd46beb9d4
Aen Jojo <josuafernando999@gmail.com>
2022-12-30 23:59:18 +0700

    feat: add word finder home page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import Heading from "../components/Heading.svelte";
+
+	let result: {
+		word: string,
+		score: number,
+		tags: string[],
+	}[] = [{
+		word: 'none',
+		score: 0,
+		tags: ['-']
+	}];
+
+	const handleSubmit = (e: any) => {
+		const actionUrl = e.target.action;
+
+		const formData = new FormData(e.target);
+		const data = new URLSearchParams();
+
+		for (const [name, value] of formData) {
+			data.append(name, value as string);
+		}
+
+		let a = fetch(`${actionUrl}?${data}`)
+			.then(res => res.json())
+			.then(data => result = data)
+			.catch(console.error);
+
+		console.log(a)
+	}
+</script>
+
+<h1 class="text-4xl font-extrabold">Word Finder</h1>
+<h2 class="text-2xl font-bold">Find a word easier</h2>
+
+<h3 class="text-xl font-semibold text-center">Find Word</h3>
+<form action="https://api.datamuse.com/words" on:submit|preventDefault={handleSubmit} class="flex flex-col gap-4 items-center">
+	<label for="words">Word with similar meaning to:</label>
+	<input name="ml" id="words" type="text" class="border border-blue-500" />
+	<input type="submit" value="Find" class="bg-blue-500 text-slate-50 px-2 cursor-pointer" />
+</form>
+
+<h3 class="text-xl font-semibold">Result:</h3>
+<ol class="list-decimal ml-4">
+	{#each result as data}
+		<li>{data.word}</li>
+	{/each}
+</ol>
