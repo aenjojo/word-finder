bdd9937aa3f327dee8272b953c3fd9f87ec11693
Aen Jojo <josuafernando999@gmail.com>
2023-01-02 22:04:26 +0700

    fix: use `if else` to handle empty result
    * fix: use `if else` to handle empty result
    
    * feat: maximize data fetched to top 10
    
    * refactor: remove default value of result

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,9 @@
 <script lang="ts">
-	import Heading from "../components/Heading.svelte";
-
 	let result: {
 		word: string,
 		score: number,
 		tags: string[],
-	}[] = [{
-		word: 'none',
-		score: 0,
-		tags: ['-']
-	}];
+	}[];
 
 	const handleSubmit = (e: any) => {
 		const actionUrl = e.target.action;
@@ -21,6 +15,8 @@
 			data.append(name, value as string);
 		}
 
+		data.append('max', '10');
+
 		let a = fetch(`${actionUrl}?${data}`)
 			.then(res => res.json())
 			.then(data => result = data)
@@ -30,8 +26,7 @@
 	}
 </script>
 
-<h1 class="text-4xl font-extrabold">Word Finder</h1>
-<h2 class="text-2xl font-bold">Find a word easier</h2>
+<h1 class="text-4xl font-extrabold">Find a word easier</h1>
 
 <h3 class="text-xl font-semibold text-center">Find Word</h3>
 <form action="https://api.datamuse.com/words" on:submit|preventDefault={handleSubmit} class="flex flex-col gap-4 items-center">
@@ -41,8 +36,12 @@
 </form>
 
 <h3 class="text-xl font-semibold">Result:</h3>
-<ol class="list-decimal ml-4">
-	{#each result as data}
-		<li>{data.word}</li>
-	{/each}
-</ol>
+{#if result}
+	<ol class="list-decimal ml-4">
+		{#each result as data}
+			<li>{data.word}</li>
+		{/each}
+	</ol>
+{:else}
+	<p>No result</p>
+{/if}
