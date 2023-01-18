c9a4eba5164537f58a5399fbbe0410fb42f18909
Aen Jojo <josuafernando999@gmail.com>
2023-01-19 01:05:05 +0700

    feat: add heading component

---
+++ src/components/Heading.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let title: string;
+	export let level: '1' | '2' = '2';
+</script>
+
+{#if level === '1'}
+	<h1 class="text-4xl font-extrabold text-center">
+		{title}
+	</h1>
+{:else if level === '2'}
+	<h2 class="text-2xl font-bold">
+		{title}
+	</h2>
+{/if}
