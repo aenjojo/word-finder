261ba02df2f86519b5b96ce7bd8f779e198fd0c0
Aen Jojo <josuafernando999@gmail.com>
2023-01-18 23:18:19 +0700

    feat: add link component

---
+++ src/components/Link.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let href: string;
+	export let value: string = '';
+</script>
+
+<a
+	href="{href}"
+	class="font-medium hover:underline underline-offset-2 decoration-2 text-blue-500 hover:text-green-500"
+>
+	{#if value}
+		{value}
+	{:else}
+		<slot name="value" />
+	{/if}
+</a>
