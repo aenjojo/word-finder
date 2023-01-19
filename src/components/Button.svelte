151d2eb5142e1f9745a8469adcd0b5a8ba30a617
Aen Jojo <josuafernando999@gmail.com>
2023-01-19 23:18:23 +0700

    feat: add button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let value: string = 'Button';
+	export let type: 'button' | 'reset' | 'submit' = 'button';
+	export let color: 'primary' | 'danger' = 'primary';
+
+	const colorData = {
+		primary: 'bg-blue-500 text-slate-50 hover:bg-blue-700',
+		danger: 'bg-red-500 text-slate-50 hover:bg-red-700',
+	};
+</script>
+
+<button
+	type={type}
+	class={`py-2 px-4 rounded-lg font-bold tracking-wider uppercase cursor-pointer active:scale-95 transition-all ${colorData[color]}`}
+>
+	{value}
+</button>
